58b5950fed05b64d5e421e16b0db2565a979318f
stmarcin <stmarcin@gazeta.pl>
2018-09-26 16:46:54 +0200

    divided into two functions: for one day long (not recommended) and for selected hours

---
+++ GTFS_EdgeWithTime_AllDay.R
@@ -0,0 +1,100 @@
+
+
+GTFS_EdgeWithTime <- function(dir_GTFS, dir_final, output = "shp"){
+  
+
+    # 1) load libraries -----------------------------------------------------------
+    Packages <- c("data.table", "chron", "lubridate", "sp", "sf")
+    lapply(Packages, library, character.only = TRUE)
+    rm(Packages)
+    
+    # 2) creates "edges" with minimum travel_time (in minutes) between stop_idO and stop_idD ----
+    
+    # Open stop_times file:
+    stop_times <- fread(paste(dir_GTFS, "stop_times.txt", sep = "/"))[,.(trip_id, stop_sequence, arrival_time, departure_time, stop_id)]
+    
+    # Create empty data table for the stop_times output; define required columns
+    Names_edges <- c("stop_idO", "stop_id", "travel_time")
+    edges <- setNames(data.table(matrix(nrow = 0, ncol = 3)), Names_edges)
+    
+    # OD nodes & travel time between them for all trips  
+    for(trip in unlist(unique(stop_times[,.(trip_id)]) )){
+    
+      # select stop_times for selected trip
+      TempST <- stop_times[trip_id == trip]  
+      
+      # in case of arrival / departure time formatted as: "24:MM:SS" (H >= 24)
+      # - add +1 day and substract 24 hours (aday and dday, respectively)
+      TempST[, c("aday", "dday") := list(ifelse(substr(arrival_time,1,2) < 24, 0, 1), ifelse(substr(departure_time,1,2) < 24, 0, 1))]
+      
+      TempST[, c("arrival_time", "departure_time") := 
+               list(ifelse(substr(arrival_time,1,2) < 24, arrival_time, 
+                           paste(paste("0", as.numeric(substr(arrival_time,1,2))-24, sep=""), substr(arrival_time,3,8), sep="")), 
+                    ifelse(substr(departure_time,1,2) < 24, departure_time,
+                           paste(paste("0", as.numeric(substr(departure_time,1,2))-24, sep=""), substr(departure_time,3,8), sep="")))]
+      
+      # calculate travel time from previous stop (travel_time, in minutes) & add stop ID of the previous stop (stop_idO)
+      TempST[, c("stop_idO", "travel_time") := list(shift(stop_id), 
+               round(as.numeric(times(chron(times = chron(times = arrival_time) + aday - 
+                                        (chron(times = shift(departure_time)) + shift(dday)))))*60*24, 2) ) ]
+      
+      # add rows (except first) to edges
+      edges <- rbind(edges, TempST[stop_sequence != 0,..Names_edges])
+      rm(TempST, trip)
+    }
+    
+    # group edges selecting minumum travel time and set names "O"=origin, "D"=destination, "travel_time" (in minutes)
+    edges <- edges[, .(min(travel_time)), by = c("stop_idO", "stop_id")]
+    setnames(edges, -1, c("stop_idD", "travel_time"))
+    
+    # remove unnecessary files
+    rm(Names_edges, stop_times)
+    
+    # 3) add stops coordinates (origin and destinations X, Y in columns OX, OY, DX, DY) ----
+    # Open stops file:
+    stops <- fread(paste(dir_GTFS, "stops.txt", sep = "/"))
+    
+    # merge orgins to gets their coordinates: origin
+    edges <- merge(edges, stops[, .(stop_id, stop_lon, stop_lat)], by.x="stop_idO", by.y = "stop_id")
+    setnames(edges, c("stop_lon", "stop_lat"), c("OX", "OY"))
+    
+    # merge orgins to gets their coordinates: destination
+    edges <- merge(edges, stops[, .(stop_id, stop_lon, stop_lat)], by.x="stop_idD", by.y = "stop_id")
+    setnames(edges, c("stop_lon", "stop_lat"), c("DX", "DY"))
+    
+    # create the directory for the output
+    dir.create(dir_final)
+    
+    # (optional) save output as txt file
+    if(output == "txt" | output == "all" ){
+      fwrite(edges, paste(dir_final, "edges.txt", sep = "/")) }
+    
+    # 4) create spatial representation and save outputs to shapefile
+    # using this stackoverflow Q&A as an inspiration: 
+    # https://stackoverflow.com/questions/20531066/convert-begin-and-end-coordinates-into-spatial-lines-in-r
+    
+    if(output == "shp" | output == "all" ){
+      # create a raw list to story lines
+      edges_sf <- vector("list", nrow(edges))
+      
+      # Create list of simple feature geometries (linestrings)
+      for (i in seq_along(edges_sf)){
+        edges_sf[[i]] <- st_linestring(as.matrix(rbind(setnames(edges[i,.(OX, OY)], 
+                                                        c("CoordX", "CoordY")), setnames(edges[i,.(DX, DY)], c("CoordX", "CoordY")))))
+      }
+      rm(i) # remove it as it is no needed anymore. 
+      
+      # Create simple feature geometry list column
+      edges_sfc <- st_sfc(edges_sf, crs = "+proj=longlat +datum=WGS84")
+      
+      # populate data attached to lines by values from original edges file:
+      edges_sf = st_sf(id = 1:nrow(edges), 
+                       travel_time = edges[,.(travel_time)], 
+                       stop_idO =  edges[,.(stop_idO)], 
+                       stop_idD =  edges[,.(stop_idD)], 
+                       geometry = edges_sfc)
+      
+      # save output as shapefile
+      st_write(edges_sf, paste(dir_final, "edges_sf.shp", sep="/"), delete_layer = TRUE) }
+    
+}
